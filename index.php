b92a34fb6c1eca3f065a76598322deb23f96300c
845 <vinci845@icloud.com>
2026-02-25 10:56:41 +0100

    test

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$TITLE = "Test";
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?= htmlspecialchars($TITLE)?></title>
+    <link href="/static/style/styles.css" rel="stylesheet"/>
+</head>
+<body>
+    <h1><?= htmlspecialchars($TITLE) ?></h1>
+</body>
+
+</html>
+
+
+
