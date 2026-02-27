4f8c4e8bc3fd732eb6839ac549bdc0844e1334bc
845 <vinci845@icloud.com>
2026-02-27 09:34:35 +0100

    Minor Changes

--- views/projects.php
+++ views/projects.php
@@ -1,1 +1,31 @@
 <?php
+$TITLE = "Projects"
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>845.lol - <?= htmlspecialchars($TITLE) ?></title>
+    <link href="../static/style/styles.css" rel="stylesheet"/>
+    <link rel="icon" type="image/svg" href="../static/assets/favicon.svg">
+</head>
+<body>
+<div class="centred">
+    <h1 class="masked-text title"><?= htmlspecialchars($TITLE) ?></h1>
+</div>
+<nav class="navbar centred">
+    <p><a href="/">Home</a> | <a href="/portfolio">Portfolio</a> | <a href="/gallery">Gallery</a> | <a href="/host">Hosting
+            Services</a>
+        <a href="/contact">Contact</a></p>
+</nav>
+<h1 class="centred">WORK IN PROGRESS</h1>
+<footer>
+    <h3 class="titlething">
+        845.lol
+    </h3>
+    <p>Made by <a href="https://github.com/u6ma/">Ale</a></p>
+    <p>Website source is available for free <a href="https://github.com/u6ma/845-personal-website">here</a>.</p>
+</footer>
+</body>
+</html>
